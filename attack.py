5cf8126a4f27ad78e9f23962928a78d663c71569
cecerhcp <cecerhcp0@hotmail.com>
2014-10-11 19:03:02 -0300

    added attack.py pokemon.py 1.0

---
+++ attack.py
@@ -0,0 +1,19 @@
+from type import Type
+
+class Attack:
+	name = None
+	typ = None
+	accu = None
+	pwr = None
+	pp = None
+
+	def __init__(self, name, typ, accu, pwr, pp):
+		self.name = name
+		self.typ = typ
+		self.accu = accu
+		self.pwr = pwr
+		self.pp = pp
+
+	def hasPP(self):
+		return self.pp > 0 and not(self.pp is None)
+	
---
+++ pokemon.py
@@ -0,0 +1,23 @@
+#to ligado que se pa esses import tem alguns redundantes (se pa, n manjo)
+from type import Type
+from attack import *
+
+class Pokemon:
+	name = None
+	level = None
+	typ1 = None
+        typ2 = None
+	hp = None
+	atk = None
+	dfs = None
+	spd = None
+	spc = None
+	atk1 = None
+	atk2 = None
+	atk3 = None
+	atk4 = None	
+
+	def __init__(self, pokefile):
+		#read pokefile ???
+
+	
