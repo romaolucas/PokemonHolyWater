348928d13849f52dac0679242ff52910035a1a96
cecerhcp <cecerhcp0@hotmail.com>
2014-10-12 01:25:09 -0300

    battle inserido e alguns .pokes

---
+++ battle.py
@@ -0,0 +1,38 @@
+import sys
+from pokemon import *
+
+argv = sys.argv
+first = Pokemon(argv[1])
+second = Pokemon(argv[2])
+print(first is second)
+
+def allAlive():
+	if first.hp <= 0 and second.hp <= 0:
+		return 'draw'
+	elif first.hp <= 0:
+		return '1st: ' + first.name
+	elif second.hp <= 0:
+		return '2nd: ' + second.name
+	else:
+		 return True
+
+	
+
+def main(argv=None):
+	active = first
+	print(first.atks is second.atks)
+	while(allAlive()):
+		print('vez do ' + active.name)
+		for i in range(0, 4):
+			print(str(i+1) + '-' + active.atks[i].name + ' - PWR: ' + str(active.atks[i].pwr) + ' - PP: ' + str(active.atks[i].pp))
+		choice = input('Escolha o ataque de seu pokemon (1-4):\n')
+		#efeitos da choice
+		if (active is first):
+			active = second
+		else:
+			active = first
+
+
+
+if __name__ == "__main__":
+    sys.exit(main())
