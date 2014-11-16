5fdb309049908a43120cfac44251cfed62dc7a9c
Lucas Romao Silva <luck.romao@gmail.com>
2014-11-16 19:27:56 -0200

    Adicionado funcionalidade que permite ler o ataque do cliente

--- pokeServer.py
+++ pokeServer.py
@@ -4,26 +4,41 @@ from lxml import etree
 
 app = Flask(__name__)
 
-@app.route('/battle/', methods = ['POST'])
-def startBattle():
-    try:
-        poke_cliente = Pokemon(request.data, xml = True) # tratar erro aqui?
+class Server():
 
-    except etree.XMLSyntaxError as e:
-        print('Erro de sintaxe no XML: ', e)
-        abort(422)
+    _poke_cliente = None
 
-    name = input()
-    print(name)
-    poke_server = Pokemon(name)
-    xml = poke_server.toXML(request.data)
-    input('escolha o ataque:')
-    return xml, 200
- 
-@app.route('/battle/attack/<int:id>', methods = ['POST'])
-def computeAttack(id):
-    print('Voce escolheu o ataque: ',id)
-    return 'GGWP', 200
+    @property
+    def poke_cliente(self):
+        return self._poke_cliente
+
+    @poke_cliente.setter
+    def poke_cliente(self, value):
+        self._poke_cliente = value
+
+    def startBattle(self):
+        try:
+            self.poke_cliente = Pokemon(request.data, xml = True) # tratar erro aqui?
+            
+        except etree.XMLSyntaxError as e:
+            print('Erro de sintaxe no XML: ', e)
+            abort(422)
+
+        name = input()
+        print(name)
+        poke_server = Pokemon(name)
+        xml = poke_server.toXML(request.data)
+        input('escolha o ataque:')
+        return xml, 200
+     
+    def computeAttack(self, id):
+        print('Voce escolheu o ataque: ', self.poke_cliente.atks[id].name)
+        return 'GGWP', 200
+
+serv = Server()
+
+app.add_url_rule('/battle/', 'startBattle', serv.startBattle, methods=['POST'])
+app.add_url_rule('/battle/attack/<int:id>', 'computeAttack', serv.computeAttack,  methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug = True)
